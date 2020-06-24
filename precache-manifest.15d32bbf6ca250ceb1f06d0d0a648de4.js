9613248d16bf26f362d283f45d48ca92216c2548
kenneththesheep <kenneththesheep@gmail.com>
2020-06-25 01:04:07 +0800

    Updates

---
+++ precache-manifest.15d32bbf6ca250ceb1f06d0d0a648de4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df0ea0963fa7c20a1326027db01c4b2",
+    "url": "/pic_react_api/index.html"
+  },
+  {
+    "revision": "76098fc511a6521bc67b",
+    "url": "/pic_react_api/static/css/main.d7cb8655.chunk.css"
+  },
+  {
+    "revision": "e33d29e69669432efcf3",
+    "url": "/pic_react_api/static/js/2.58488f5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pic_react_api/static/js/2.58488f5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76098fc511a6521bc67b",
+    "url": "/pic_react_api/static/js/main.0e0c6b4a.chunk.js"
+  },
+  {
+    "revision": "75cdc351050dc797a649",
+    "url": "/pic_react_api/static/js/runtime-main.8d566176.js"
+  }
+]);
